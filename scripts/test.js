6384ea2a45b79ec5a3236250a057089c228a4dcf
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-01-09 19:49:22 +0100

    dont watch tests

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,7 @@
 const execa = require('execa')
+const args = ['--config', 'jest.config.js']
 
-execa('jest', ['--config', 'jest.config.js', '--watchAll'], {
+execa('jest', args, {
   stdio: 'inherit',
   env: {
     WITH_BUILD: process.argv.some(v => v === '--build')
