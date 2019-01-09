0c36548752ce272b8d4d6d5b415395dbd8bb34ec
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-01-09 19:56:07 +0100

    run jest directly

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,3 @@
-const execa = require('execa')
 const args = ['--config', 'jest.config.js']
 
-execa('jest', args, {
-  stdio: 'inherit',
-  env: {
-    WITH_BUILD: process.argv.some(v => v === '--build')
-  }
-})
+require('jest').run(args)
